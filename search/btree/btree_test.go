ae778342fc8de9655f388e7dba9343fef897b413
WangFang <hju_wang@126.com>
2023-01-24 22:04:06 +0800

    写完Btree测试

--- search/btree/btree_test.go
+++ search/btree/btree_test.go
@@ -4,6 +4,15 @@ import "testing"
 
 func TestBtree(t *testing.T) {
 
+	r := []int{22, 16, 41, 58, 8, 11, 12, 16, 17, 22, 23, 31, 41, 52, 58, 59, 61}
+
+	var T BTree
+	var s Result
+	for i := 0; i < len(r); i++ {
+		//查找位置
+		s = SearchBtree(T, r[i])
+		if s.tag == 0 {
+			InsertBtree(&T, r[i], s.pt, s.i)
+		}
+	}
 }
-
-
